428e714c59a7ece8d6daa85bc08b6531ce85a051
Mishal Pandey <91657594+MishalPandey@users.noreply.github.com>
2024-02-25 15:54:48 +0530

    Update Util.java

--- app/src/main/java/com/mishalpandey/donorsbook4r/Email/Util.java
+++ app/src/main/java/com/mishalpandey/donorsbook4r/Email/Util.java
@@ -2,5 +2,5 @@ package com.mishalpandey.donorsbook4r.Email;
 
 public class Util {
     public static final String EMAIL ="mishalpandey1@gmail.com";
-    public static final String PASSWORD="aeekpubbotjxyjrm";
+    public static final String PASSWORD="";
 }
